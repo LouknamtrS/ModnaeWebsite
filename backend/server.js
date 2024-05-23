c7ae69dfec5c8950554e48870a22bd683a6a50e3
Thidarat <111549082+LouknamtrS@users.noreply.github.com>
2024-05-24 03:37:40 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ connection.once("open", () => {
     console.log("MongoDB database connected.");
 });
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
